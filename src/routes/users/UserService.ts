0b9633d706db394b24fb08f416ced6b6744318f9
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-07-22 08:01:16 -0300

    Atualizações no services

--- src/routes/users/UserService.ts
+++ src/routes/users/UserService.ts
@@ -1,14 +1,20 @@
 import Password from "../../utils/Password";
+import Token from "../../utils/Token";
 import { IUserRegister } from "./UserInterface";
 import UserRepository from "./UserRepository";
 
+
+const TEST_ROLE = String(process.env.TEST_ROLE) || 'user' 
+
 class UserService {
+
     public Create = async (data: IUserRegister) => {
+
         if (!/^[a-zA-Z][a-zA-Z0-9._]{2,19}$/.test(data.name)) {
             throw new Error('Nome inválido. O nome deve conter apenas letras, números, pontos e sublinhados, e deve ter entre 3 e 20 caracteres.')
         }
         if (!/^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,50}$/.test(data.email)) {
-             throw new Error('Email inválido. O Email deve estar no formato usuario@dominio.extensão.')
+             throw new Error('Email inválido. O Email deve seguir o formato padrão.')
         }
         if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,50}$/.test(data.password)) {
              throw new Error('Senha inválida. A senha deve ter entre 8 e 50 caracteres e conter pelo menos uma letra, um número e um caractere especial.')
@@ -24,4 +30,21 @@ class UserService {
             password: passwordHash
         })
     }
+
+    public login = async (email:string, password:string) => {
+
+        const user = await UserRepository.Email(email);
+
+        if (!user) {
+            throw new Error('Email de usuario não encontrado')
+        }
+
+        const isPasswordValid = await Password.Compare(password, user.password)
+        if(!isPasswordValid) {
+            throw new Error('Senha inválida')
+        }
+
+        return Token.generate({userId: user.id, role: TEST_ROLE})
+    }
 }
+export default new UserService();
