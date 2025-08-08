ec64445ed91ba862408ea13d01a470f4a97c9bd7
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-08-08 07:56:22 -0300

    voltei o cors para testes

--- src/App.ts
+++ src/App.ts
@@ -15,7 +15,7 @@ class App {
 
     private CorsOptions = () => {
         this.express.use(cors({
-            origin: 'https://password-guard-new.vercel.app/',
+            origin: '*',
             methods: 'GET, PUT, POST, DELETE',
             allowedHeaders: 'Content-Type, Authorization',
             exposedHeaders: 'Content-Type, Authorization'
